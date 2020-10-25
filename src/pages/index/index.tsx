8137462fcf2b33a2680829265e9471b826a6fda7
zangchuqiang <zangchuqiang@chengfayun.com>
2020-10-25 20:58:55 +0800

    chore: update index page

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,63 +1,52 @@
-import React, { Component } from 'react'
-import { View, Button, Text } from '@tarojs/components'
-import { observer, inject } from 'mobx-react'
+import React, { Component } from "react";
+import { View, Button, Text } from "@tarojs/components";
+import { observer, inject } from "mobx-react";
 
-import './index.less'
+import "./index.less";
 
 type PageStateProps = {
-  store: {
-    counterStore: {
-      counter: number,
-      increment: Function,
-      decrement: Function,
-      incrementAsync: Function
-    }
-  }
-}
+  // store: {
+  //   counterStore: {
+  //     counter: number,
+  //     increment: Function,
+  //     decrement: Function,
+  //     incrementAsync: Function
+  //   }
+  // }
+};
 
 interface Index {
-  props: PageStateProps;
+  // props: PageStateProps;
 }
 
-@inject('store')
+@inject("store")
 @observer
 class Index extends Component {
-  componentWillMount () { }
+  componentWillMount() {}
 
-  componentDidMount () { }
+  componentDidMount() {}
 
-  componentWillUnmount () { }
+  componentWillUnmount() {}
 
-  componentDidShow () { }
+  componentDidShow() {}
 
-  componentDidHide () { }
+  componentDidHide() {}
 
-  increment = () => {
-    const { counterStore } = this.props.store
-    counterStore.increment()
-  }
-
-  decrement = () => {
-    const { counterStore } = this.props.store
-    counterStore.decrement()
-  }
-
-  incrementAsync = () => {
-    const { counterStore } = this.props.store
-    counterStore.incrementAsync()
-  }
-
-  render () {
-    const { counterStore: { counter } } = this.props.store
+  render() {
     return (
-      <View className='index'>
-        <Button onClick={this.increment}>+</Button>
-        <Button onClick={this.decrement}>-</Button>
-        <Button onClick={this.incrementAsync}>Add Async</Button>
-        <Text>{counter}</Text>
+      <View className="index">
+        <View className="year">
+          <Text>二零二零年</Text>
+          <Text>撰</Text>
+        </View>
+        <View className="month">
+          <Text>一月</Text>
+          <Text>二月</Text>
+        </View>
+        <Text>设</Text>
       </View>
-    )
+    );
   }
 }
 
-export default Index
+export default Index;
